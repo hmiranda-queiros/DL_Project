f89b9c6ef45b1422d29e35e49950f11eda10c37b
Hugo MIRANDA QUEIROS <hugomirandaqueiros@gmail.com>
2022-04-24 15:30:10 +0200

    first commit

---
+++ Miniproject_2/others/test.py
@@ -0,0 +1,2 @@
+def oui():
+    print("coucou")
---
+++ eval.py
@@ -0,0 +1,16 @@
+import Miniproject_1
+import Miniproject_2
+
+
+def main():
+    test1 = Miniproject_1.Model()
+    print(test1)
+    Miniproject_1.test.oui()
+
+    test2 = Miniproject_2.Model()
+    print(test2)
+    Miniproject_2.test.oui()
+
+
+if __name__ == "__main__":
+    main()
