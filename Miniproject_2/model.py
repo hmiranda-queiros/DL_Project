6a404f461d0d57fdf3014d7554fc3e7b23a1d72d
Hugo MIRANDA QUEIROS <hugomirandaqueiros@gmail.com>
2022-04-30 20:23:00 +0200

    project 2 continued

--- Miniproject_2/model.py
+++ Miniproject_2/model.py
@@ -1,4 +1,5 @@
 import torch
+import time
 
 from .src import module as m
 from .src import denoiser
@@ -8,38 +9,77 @@ torch.set_grad_enabled(False)
 
 
 # For mini-project 2
+def psnr(denoised, ground_truth):
+    # Peak Signal to Noise Ratio : denoised and ground_truth have range [0, 1]
+    mse = torch.mean((denoised - ground_truth) ** 2)
+    return -10 * torch.log10(mse + 10 ** -8)
+
+
 class Model:
-    def __init__(self) -> None:
+    def __init__(self, mini_batch_size) -> None:
         # instantiate model + optimizer + loss function + any other stuff you need
         self.model = denoiser.Denoiser()
         self.criterion = m.MSELoss()
-        self.optimizer = m.SGD(self.model.parameters(), lr=1e-1)
-        self.mini_batch_size = 5
+        self.lr = 0.001
+        self.optimizer = m.SGD(self.model.parameters(), lr=self.lr)
+        self.mini_batch_size = mini_batch_size
 
     def load_pretrained_model(self) -> None:
         # This loads the parameters saved in bestmodel.pth into the model
-        denoiser_state_dict = torch.load('./Miniproject_2/bestmodel.pth')
-        self.model.load_state_dict(denoiser_state_dict)
+        pass
 
-    def train(self, train_input, train_target) -> None:
+    def train(self, train_input, train_target, num_epochs, test_input, test_target) -> None:
         #: train_input : tensor of size (N, C, H, W) containing a noisy version of the images.
         #: train_target : tensor of size (N, C, H, W) containing another noisy version of the same images,
         # which only differs from the input by their noise .
-        """
+        print(f"Starts Training with : mini_batch_size = {self.mini_batch_size} and num epochs = {num_epochs}")
+        total_time = 0
+        nb_step = 0
+        result = 0
+
         for e in range(num_epochs):
             for b in range(0, train_input.size(0), self.mini_batch_size):
+                start = time.time()
+
                 output = self.model(train_input.narrow(0, b, self.mini_batch_size))
                 loss = self.criterion(output, train_target.narrow(0, b, self.mini_batch_size))
                 self.model.zero_grad()
-                loss.backward()
+                self.model.backward(loss.backward())
                 self.optimizer.step()
-        """
+                nb_step += 1
+
+                end = time.time()
+                total_time += end - start
+
+                if nb_step % 100 == 0:
+                    result = self.validation(test_input, test_target)
+
+                    print(
+                        f"Epoch number : {e + 1}, Step number : {nb_step}, PSNR : {result:.2f},"
+                        f" mini_batch_size = {self.mini_batch_size}, Total running time : {total_time:.1f} s")
+
+                if total_time > 10 * 60:
+                    print("10 mins exceeded")
+                    return None
+
+            # adapt minibatch_size
+            if result >= 23:
+                self.mini_batch_size = 25
+
+            if result >= 24:
+                self.mini_batch_size = 25
+
+        print(f"End of training with total running time : {total_time:.1f} s")
 
     def predict(self, test_input) -> torch.Tensor:
         #: test_input : tensor of size (N1 , C, H, W) that has to be denoised by the trained
         # or the loaded network.
         #: returns a tensor of the size (N1 , C, H, W)
         output = torch.empty(0).to(device)
-        for b in range(0, test_input.size(0), self.mini_batch_size):
-            output = torch.cat((output, self.model(test_input.narrow(0, b, self.mini_batch_size))), 0)
+        for b in range(0, test_input.size(0), 100):
+            output = torch.cat((output, self.model(test_input.narrow(0, b, 100))), 0)
         return output
+
+    def validation(self, test_input, test_target):
+        output = self.predict(test_input)
+        return psnr(output, test_target).item()
--- eval_2.py
+++ eval_2.py
@@ -1,8 +1,11 @@
 import Miniproject_2
 import torch
+import matplotlib.pyplot as plt
+import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.set_grad_enabled(False)
+
 
 def psnr(denoised, ground_truth):
     # Peak Signal to Noise Ratio : denoised and ground_truth have range [0, 1]
@@ -11,19 +14,18 @@ def psnr(denoised, ground_truth):
 
 
 def main():
-    global load, save, nb_samples, num_epochs
+    global load, save, nb_samples, num_epochs, mini_batch_size
 
     # Load the data
     noisy_imgs_1, noisy_imgs_2 = torch.load('./data/train_data.pkl', map_location=device)
     noisy_imgs, clean_imgs = torch.load('./data/val_data.pkl', map_location=device)
-    print(noisy_imgs_1.shape)
-    print(noisy_imgs_1.device)
 
     # Converts the data to float type
-    noisy_imgs_1 = noisy_imgs_1[: nb_samples].float()
-    noisy_imgs_2 = noisy_imgs_2[: nb_samples].float()
-    noisy_imgs = noisy_imgs[: nb_samples].float()
-    clean_imgs = clean_imgs[: nb_samples].float()
+    noisy_imgs_1 = noisy_imgs_1.float()
+    noisy_imgs_2 = noisy_imgs_2.float()
+
+    noisy_imgs = noisy_imgs.float()
+    clean_imgs = clean_imgs.float()
 
     # Normalisation of data
     noisy_imgs_1.div_(255)
@@ -31,34 +33,58 @@ def main():
     noisy_imgs.div_(255)
     clean_imgs.div_(255)
 
-    denoiser = Miniproject_2.Model()
+    denoiser = Miniproject_2.Model(mini_batch_size)
 
-    if not load:
-        # Training
-        denoiser.train(noisy_imgs_1, noisy_imgs_2)
+    print(f"Device used : {noisy_imgs_1.device}")
+    print(f"Input shape : {noisy_imgs_1.shape}")
 
-    else:
+    if load:
         # Load the saved best model
         denoiser.load_pretrained_model()
 
-    # Validation
-    output = denoiser.predict(noisy_imgs)
-    print(output.shape)
-    noise_db = psnr(output, clean_imgs).item()
-    print(noise_db)
+    else:
+        # Training
+        denoiser.train(noisy_imgs_1, noisy_imgs_2, num_epochs, noisy_imgs, clean_imgs)
 
-    # Noise without filtering
+    # Validation
+    print("Starts Validation")
+    output = denoiser.predict(noisy_imgs)
+    print(f"Output shape : {output.shape}")
+    noise_db = psnr(output, clean_imgs).item()
+    print(f"Noise after denoising : {noise_db:.2f}")
+
+    # Noise without denoising
     noise_db = psnr(noisy_imgs, clean_imgs).item()
-    print(noise_db)
+    print(f"Noise without denoising : {noise_db:.2f}")
 
     if save:
         # Save the model
         torch.save(denoiser.model.state_dict(), './Miniproject_1/bestmodel.pth')
 
+    # Plots the result
+    images_clean = clean_imgs[:5].mul(255).to("cpu").numpy()
+    images_noisy = noisy_imgs[:5].mul(255).to("cpu").numpy()
+    images_filtered = output[:5].mul(255).detach().to("cpu").numpy()
+
+    fig = plt.figure(figsize=(25, 4))
+    for i in range(3):
+        for idx in range(5):
+            fig.add_subplot(3, 5, idx + 1 + 5 * i, xticks=[], yticks=[])
+
+            if i == 0:
+                plt.imshow(np.transpose(images_clean[idx], (1, 2, 0)).astype('uint8'))
+
+            elif i == 1:
+                plt.imshow(np.transpose(images_noisy[idx], (1, 2, 0)).astype('uint8'))
+
+            else:
+                plt.imshow(np.transpose(images_filtered[idx], (1, 2, 0)).astype('uint8'))
+    plt.show()
+
 
 if __name__ == "__main__":
     load = False
     save = False
-    nb_samples = 10
-    num_epochs = 5
+    num_epochs = 2
+    mini_batch_size = 50
     main()
